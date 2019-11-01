237f79e2cbf0c82282c06227b8c705309ce8d058
Muhammad Naufal <muhammad_naufal@hotmail.com>
2019-11-01 15:06:49 +0700

    Let and Const

--- main.js
+++ main.js
@@ -1,1 +1,6 @@
-console.log('Hello World')
+const asal = "Aceh"
+let nama = "Muhammad Naufal"
+let kampus = "Universitas Komputer Indonesia"
+let semester = 1;
+
+console.log(`Hallo, nama saya ${nama}, saya berasal dari ${asal}, saat ini sedang kuliah di ${kampus} semester ${semester}`)
