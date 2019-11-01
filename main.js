d56099b500655083ccbb16811e99f21ecb943edd
Muhammad Naufal <muhammad_naufal@hotmail.com>
2019-11-01 15:16:53 +0700

    Arrow Function

--- main.js
+++ main.js
@@ -1,6 +1,15 @@
-const asal = "Aceh"
-let nama = "Muhammad Naufal"
-let kampus = "Universitas Komputer Indonesia"
-let semester = 1;
+const printInfoMahasiswa = (nama, nim, prodi) => {
+  console.log(`Nama: ${nama}, NIM: ${nim}, Prodi: ${prodi}`)
+}
 
-console.log(`Hallo, nama saya ${nama}, saya berasal dari ${asal}, saat ini sedang kuliah di ${kampus} semester ${semester}`)
+const perkalian = (a, b) => {
+  return a * b;
+}
+
+const pembagian = (a, b) => a / b;
+
+printInfoMahasiswa("Naufal", "10517095", "Sistem Informasi")
+let hasilPerkalian = perkalian(10, 5)
+console.log(hasilPerkalian)
+let hasilPembagian = pembagian(20, 5)
+console.log(hasilPembagian)
